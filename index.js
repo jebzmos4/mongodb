403458e939dc00a2118b569282acff675f966986
Morifeoluwa Jebutu <jebzmos4@gmail.com>
2018-03-07 20:53:05 +0100

    added delete functionality

--- index.js
+++ index.js
@@ -23,16 +23,7 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
     //     if (err) {
     //         return console.log('unable to add user', err);
     //     }
-    //     console.log(b.collection('Users').insertOne({
-    //     name: 'Morfeoluwa Jebutu',
-    //     age: 23,
-    //     location: 'Lagos'
-    // }, (err, result) => {
-    //     if (err) {
-    //         return console.log('unable to add user', err);
-    //     }
     //     console.log(JSON.stringify(result.ops, undefined, 2));
-    // })JSON.stringify(result.ops, undefined, 2));
     // })
 
     // db.collection('Todos').find().toArray().then((result) => {
@@ -42,12 +33,17 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
     //     console.log('Unable to fetch todos', err);
     // });
 
-    db.collection('Todos').find().count((count, erro) => {
-        if (err) {
-            return console.log('there is an error', erro);
-        }
-        console.log(`The number of todos is: ${count}`);
-    });
+    // db.collection('Todos').find().count((err, count) => {
+    //     if (err) {
+    //         return console.log('there is an error', err);
+    //     }
+    //     console.log(`The number of todos is: ${count}`);
+    // });
 
-    db.close();
+    db.collection('Users').deleteOne({
+        name: 'Taye'
+    }).then((result) => {
+        console.log(JSON.stringify(result))
+    });
+    // db.close();
 });
